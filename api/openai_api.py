f261ee7fbeb4b09503571e3a91c3b62b05463059
liminghao <lmh1230612306@163.com>
2024-01-21 05:19:01 -0800

    Adjusted Templates

--- api/openai_api.py
+++ api/openai_api.py
@@ -21,8 +21,8 @@ def query_chatgpt3_5(content: str, additional_prompt: str) -> ChatCompletionMess
 You will summarize provided content using a title and multiple bullet points and convert the content to the following JSON format. You will also follow an additional prompt provided by the user at the end of this input, which will augment or elaborate on some of the instructions provided below. If the user does not provide an additional prompt, you may ignore it. 
 The summary will follow the following specifications:
 - The summary will cover the text in the order that it is presented. 
-- The summary will generate a relevant title that is AT MOST 15 characters
-- The summary will provide 2-5 bullet points in each slide that is relevant to the title
+- The summary will generate a relevant title that is AT MOST 10 characters
+- The summary will provide 2-4 bullet points in each slide that is relevant to the title
 You will need to replace the text in square brackets [] with your generated text. Here is the format of the JSON that you will need to convert to:
 {
    "presentation theme": "[theme]", 
@@ -34,7 +34,7 @@ You will need to replace the text in square brackets [] with your generated text
 Here is the content, surrounded by curly braces:
 {
                 """
-                + content[:1000] +
+                + content[:3000] +
                 """
 }
 The additional prompt will now be provided in curly braces. If there is nothing in the curly braces, you may ignore the additional prompt. Here is the additional prompt: {                
