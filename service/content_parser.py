fb07418d0a953ad6011615333cba1efc1acc425b
Neil Lin <neillin164@gmail.com>
2024-01-21 03:22:02 -0800

    lskjf kill mee

--- service/content_parser.py
+++ service/content_parser.py
@@ -4,6 +4,7 @@ import logging
 from docx import Document
 from fastapi import UploadFile
 
+from api.google_cloud_api import detect_text_in_image
 from service.doc_parser import extract_doc_content
 
 
@@ -18,5 +19,11 @@ def parse_content(file: UploadFile) -> str:
         return file.file.read().decode("utf-8")
     elif filename.endswith("md"):
         pass
+    elif filename.endswith("png"):
+        return detect_text_in_image(UploadFile)
+    elif filename.endswith("jpg"):
+        return detect_text_in_image(UploadFile)
+    elif filename.endswith("jpeg"):
+        return detect_text_in_image(UploadFile)
     else:
         raise ValueError("Unsupported file")
